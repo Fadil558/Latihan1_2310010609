6498a24232205f77fc91731cfa69cb9cf788a9cb
fadil558 <rujaklah@gmail.com>
2025-06-10 00:08:50 +0800

    membuat class matematikaberaksi

--- src/hitung/matematikaberaksi.java
+++ src/hitung/matematikaberaksi.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package hitung;
 
-/**
- *
- * @author LENOVO
- */
 public class matematikaberaksi {
-    
+    public static void main(String[] args) {
+        matematika fadil = new matematika(6,9);
+        
+        System.out.println("Hasil Penjumlahan: "+fadil.setpenjumlahan());
+        System.out.println("Hasil Pengurangan: "+fadil.setpengurangan());
+        System.out.println("Hasil Perkalian: "+fadil.setperkalian());
+        System.out.println("Hasil Pembagian: "+fadil.setpembagian());
+    }
 }
